37b5508aef1748af7d374c7728859026267aac6a
r-a-j-s-i-n-h-a <sinha.raj1236@gmail.com>
2021-12-03 23:32:02 +0530

    Initial commit

---
+++ src/REdemo.java
@@ -0,0 +1,19 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class REdemo
+{
+    public static void main(String[] args) {
+        String str= " the quick jdbc brown foxx jumps jdbc";
+        Pattern p= Pattern.compile("jdbc");//pattern provided here
+        Matcher m= p.matcher(str); //target string provided here
+        int ctr=0;
+        while (m.find())
+        {
+            System.out.println(m.start() + "---------"+m.end()+ "---------"+ m.group());
+            ctr++;
+        }
+        System.out.println("no of counts==" + ctr);
+    }
+
+}
