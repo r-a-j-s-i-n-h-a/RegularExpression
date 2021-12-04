de69a741eb72bc6dd0fcd678e44c44afa8b6671e
r-a-j-s-i-n-h-a <94060322+r-a-j-s-i-n-h-a@users.noreply.github.com>
2021-12-04 08:11:05 +0530

    Create REdemo3.java

---
+++ src/REdemo3.java
@@ -0,0 +1,10 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class REdemo3 {
+    public static void main(String[] args) {
+        String str = "neer";
+        System.out.println(str.matches("[a-z]{4}"));
+    }
+}
